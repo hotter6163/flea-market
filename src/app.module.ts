2fadeda6ac09d7dd8fe51efb7be552ef3f042b03
yoshikazu hotta <95465875+hotter6163@users.noreply.github.com>
2024-06-21 18:42:02 +0900

    finish chapter 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
-  controllers: [],
   providers: [],
+  controllers: [],
+  imports: [ItemsModule],
+  exports: [],
 })
 export class AppModule {}
